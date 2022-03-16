01fae6656eabaa71515d678a629e5cdd06d75cb8
Artem Laptiev <artuon@gmail.com>
2022-03-16 11:56:49 +0200

    intersection starting

---
+++ TS Syntax Playground/src/intersections.ts
@@ -0,0 +1,19 @@
+interface BusinessPartner {
+  name: string;
+  credit: number;
+}
+
+interface Identity {
+  id: number;
+  name: string;
+}
+
+interface Contact {
+  email: string;
+  phone: string;
+}
+
+type Employee = Identity & Contact;
+type Customer = BusinessPartner & Contact;
+
+export {};
